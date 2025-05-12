349c45420a3d3f7393f3657683d79ed360c31a2c
uzer <uzerpathan2004@gmail.com>
2025-05-12 12:13:45 +0530

    user routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,21 @@
+import User from "../models/user.model.js"
+
 export const getSuggestedConnections = async(req,res) =>{
-  
+  try {
+    const currentUser = await User.findById(res.User._id).select("connections");
+
+    const suggestedUser = await User.find({
+    _id:{
+      $ne: res.user._id,
+      $nin: currentUser.connections
+    }
+    })
+    .select("name username profilePicture headline")
+    .limit(5);
+
+    res.json(suggestedUser);
+  } catch (error) {
+    console.error("Error in getSuggestedConnetion Controller:", error);
+    res.status(500).json({message:"Server error"});
+  }
 }
