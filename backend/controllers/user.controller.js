a5e1e87a0167827e9f1d390d7d530e21fd6fd0ad
uzer <uzerpathan2004@gmail.com>
2025-05-12 13:33:16 +0530

    userUpdate

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -32,3 +32,34 @@ export const getPublicProfile = async(req,res) =>{
     res.status(500).json({message:"Server Error"});
   }
 }
+
+export const updateProfile = async(req,res) =>{
+  try {
+    const allowedFields =[
+      "name",
+      "headline",
+      "about",
+      "location",
+      "ProfilePicture",
+      "BannerImg",
+      "skills",
+      "experience",
+      "education",
+    ];
+
+    const updatedData = {};
+
+    for(const field of allowedFields){
+      if(req.body[field]){
+        updatedData[field] = req.body[field];
+      }
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id, {$set: updatedData}, {new: true}).select("-password");
+
+    res.json(user);
+  } catch (error) {
+    console.error("Error in getPublicProfile", error);
+    res.status(500).json({message:"Server Error"});
+  }
+}
