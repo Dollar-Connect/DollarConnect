13c415354c3a31470b3b062641c740c8d2f9d2b7
uzer <uzerpathan2004@gmail.com>
2025-05-14 14:37:28 +0530

    postroutes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import Post from "../models/post.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getFeedPosts = async (req,res) =>{
   try {
@@ -11,5 +12,32 @@ export const getFeedPosts = async (req,res) =>{
   } catch (error) {
    console.error("Error in getFeedPosts", error);
    res.status(500).json({message:"Server error"});
+  }
+}
+
+export const createPosts =async (req,res) =>{
+  try {
+    const {content, image} = res.body;
+
+    let newPost;
+
+    if (image) {
+      const imgResult = await cloudinary.uploader.upload(image)
+      newPost = new Post({
+        author:req.user._id,
+        content,
+        image:imgResult.secure_url
+      });
+    } else {
+      newPost = new Post({
+        author:req.user._id,
+        content
+      });
+    }
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    
   }
 }
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { protectRoute} from "../middleware/auth.middleware.js";
+import { createPosts, getFeedPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.get("/",protectRoute,getFeedPosts);
+router.get("/",protectRoute, getFeedPosts);
+router.post("/create", protectRoute, createPosts)
 
 export default router;
